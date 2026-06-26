1937c23a0725e419595cbcf2f9b53ee102a350e5
SamratDebugger <md721023@gmail.com>
2026-06-26 20:41:54 +0600

    hero running

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+import { FaPlay } from "react-icons/fa6";
+
+const heroTexts = ["Business Grow", "Life Easier", "Work Better"];
 export default function Hero() {
+  const [heroText, setHeroText] = useState(heroTexts[0]);
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 min-h-[calc(100dvh-89px)]">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+        <div className="max-w-2xl">
+          <h1 className="text-6xl font-bold">
+            Everything we do to make{" "}
+            <span className="text-info">{heroText}.</span>
+          </h1>
+          <p className="py-6 text-balance">
+            Find the right space, services, and support, so your business and
+            its people can work their way, every day.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <div className="flex justify-center gap-5 items-center">
+            <button className="btn btn-info">Book Now</button>
+            <div>
+              <button className="btn text-info btn-circle shadow-xl">
+                <FaPlay />
+              </button>
+              <span className="ml-2">How It Works</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
